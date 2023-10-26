59abcef27a282284ad087f484a37d0b7af824174
Fabunmi Ebenezer <fabunmiebenezer37@gmail.com>
2023-10-26 12:14:19 +0100

    add the function to display the operator button

--- js/script.js
+++ js/script.js
@@ -5,12 +5,7 @@ $(document).ready(function () {
      $(".display").val(currentInputtedNumber + number);
   });
  
-  // Display the value of the operator
-  // $(".operator").click(function () {
-  //    let operator = $(this).text();
-  //    let currentInputtedNumber = $(".display").val();
-  //    $(".display").val(currentInputtedNumber + operator);
-  // });
+
   // $(".reset").click(function(){
   //   $(".display").val("");
   // })
