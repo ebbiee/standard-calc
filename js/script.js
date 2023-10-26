452c567697f3d9bb6de0a6fad0f6b1d19e997e0f
Fabunmi Ebenezer <fabunmiebenezer37@gmail.com>
2023-10-26 13:04:52 +0100

    finssh writing all the code without errors

--- js/script.js
+++ js/script.js
@@ -6,13 +6,28 @@ $(document).ready(function () {
   });
  
   // Display the value of the operator
-  // $(".operator").click(function () {
-  //    let operator = $(this).text();
-  //    let currentInputtedNumber = $(".display").val();
-  //    $(".display").val(currentInputtedNumber + operator);
-  // });
-  // $(".reset").click(function(){
-  //   $(".display").val("");
-  // })
-  // $("#equalsTo").click
+  $(".operator").click(function () {
+     let operator = $(this).text();
+     let currentInputtedNumber = $(".display").val();
+     $(".display").val(currentInputtedNumber + operator);
+  });
+  //clear the whole input
+  $(".reset").click(function(){
+    $(".display").val("");
+  })
+  //clear the last inputted number
+  $(".clear").click(function(){
+    let currentInputtedNumber = $(".display").val();
+    $(".display").val(currentInputtedNumber.slice(0, -1));
+  })
+  //display the value of the result
+  $("#equalsTo").click(function(){
+    try {
+      let inputtedNumberAndOperator = $(".display").val();
+      let result = eval(inputtedNumberAndOperator);
+      $(".display").val(result);
+  } catch (error) {
+      $(".display").val("Error");
+  }
+  })
  });
